ee4b7014e1b9adabf667e19b0274c453541037b6
Evgenii <evtatdevelop@gmail.com>
2023-08-24 08:58:31 +0700

    add some features

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# node-blackjack terminal app
+# check relative currency rates (terminal web sires parser)
 
 ## Install:
 
 This app use:
+
 - [node-console-colors](https://github.com/damien-otis/node-console-colors)
 - [request](https://www.npmjs.com/package/request)
 - [cheerio](https://www.npmjs.com/package/cheerio)
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 
-const bath = 9956;
+const {bathIsThere, thaiMonth, rublesIsThere, condoMonthRent} = require('./config.json');
 
 const stat = addStat => fs.exists('./stat.json', exists => {
   if ( exists ) fs.readFile('./stat.json', (err, data) => {  
@@ -21,18 +21,19 @@ const getInterest = readline.createInterface({
   output: process.stdout
 });
 
-getInterest.question('Month expenses in Thai, THB (20 000)? ', (thai_month) => 
-  getInterest.question(`How much RUB is there (300 000)? `, (money) => {
-    if ( thai_month && money ) parseRates( thai_month, money );
-    else parseRates( 20000, 300000 );
+const moneyFormat = (num, curr=null) => {
+  symbols = {'rub': '₽', 'cny': '¥ ', 'usd': '$  ', 'thb': '฿'};
+  return `${curr ? symbols[curr] : ''} ${Math.floor(num).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`
+};
+
+getInterest.question(`Month expenses in Thai, THB (${moneyFormat(thaiMonth)} ): `, thai_month => 
+  getInterest.question(`How much RUB is there (${moneyFormat(rublesIsThere)} ): `, rubles => {
+    if ( !thai_month ) thai_month = thaiMonth;
+    if ( !rubles ) rubles = rublesIsThere;
+    parseRates( thai_month, rubles );
     getInterest.close();
   })
 );
-
-const moneyFormat = (num, curr) => {
-  symbols = {'rub': '₽', 'cny': '¥ ', 'usd': '$  ', 'thb': '฿'};
-  return `${symbols[curr]} ${Math.floor(num).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`
-};
 
 const parseRates = ( thai_month, rubles ) => {
   request('https://pattaya-city.ru/banki/kurs', (err, response, html) => {
@@ -44,18 +45,22 @@ const parseRates = ( thai_month, rubles ) => {
           const { cny_rub, usd_rub } = atbSu(html);
 
           stat( { [Date.now()]: { bath_usd, bath_cny, bath_rub, cny_rub, usd_rub, }, } ); 
-
+          
           const yuan = rubles / cny_rub;
-          const dollars = rubles / usd_rub;;
+          const dollars = rubles / usd_rub;
           const rubToBath = bath_rub * rubles;
           const cnyToBath = bath_cny * yuan;
           const usdToBath = bath_usd * dollars;
           const baths = val => cc.set(  Math.max(rubToBath, cnyToBath, usdToBath) === val  ? 'fg_green' : 'fg_cyan', moneyFormat(val, 'thb') );
-          const stay = val => cc.set('fg_yellow', Math.floor((val + bath) / thai_month) + ' month')
+          const stay = val => cc.set('fg_yellow', Math.floor((val + bathIsThere) / thai_month) + ' month')
+
           console.log('');
           console.log(`RUB - ${moneyFormat(rubles, 'rub')} - ${baths(rubToBath)} (${stay(rubToBath)})`);
           console.log(`CNY - ${moneyFormat(yuan, 'cny')} - ${baths(cnyToBath)} (${stay(cnyToBath)})`);
           console.log(`USD - ${moneyFormat(dollars, 'usd')} - ${baths(usdToBath)} (${stay(usdToBath)})`);
+          
+          const weekExpss = (val) =>  Math.floor((val + bathIsThere - condoMonthRent * 6) / (181 / 7) )
+          console.log(`\n${ moneyFormat(Math.round((weekExpss(rubToBath) + weekExpss(cnyToBath) + weekExpss(usdToBath)) / 3), 'thb') } a week`);
         }  
       });
     }
@@ -80,10 +85,8 @@ const pattayaCityRu = html => {
 }
 
 const atbSu = html => {
-  let cny_rub_arch = 13.17;
-  let usd_rub_arch = 94.38;
   const $ = cheerio.load(html);          
-  cny_rub = parseFloat($('#currencyTab1').find('.currency-table').find('.currency-table__tr').eq(1).find('.currency-table__td').eq(2).text()) || cny_rub_arch;
-  usd_rub = parseFloat($('#currencyTab1').find('.currency-table').find('.currency-table__tr').eq(2).find('.currency-table__td').eq(2).text()) || usd_rub_arch;
+  cny_rub = parseFloat($('#currencyTab1').find('.currency-table').find('.currency-table__tr').eq(1).find('.currency-table__td').eq(2).text());
+  usd_rub = parseFloat($('#currencyTab1').find('.currency-table').find('.currency-table__tr').eq(2).find('.currency-table__td').eq(2).text());
   return {cny_rub, usd_rub}
 }
